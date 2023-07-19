3ae074df6d8ac3ccde3fcdf246abc84cab56c208
Sibabalwe Ngqoyiya <sngqoyiya@plusonex.com>
2023-07-19 12:37:08 +0200

    template with vars

--- task-definitions.tf
+++ task-definitions.tf
@@ -4,10 +4,10 @@ resource "aws_ecs_task_definition" "this" {
   requires_compatibilities = [var.runtime]
   network_mode             = var.runtime == "FARGATE" ? "awsvpc" : "brigde"
   container_definitions = templatefile("./td.json.tpl", {
-    # name      = each.value.task_definition.name
-    # image     = each.value.task_definition.image
-    # cpu       = each.value.task_definition.cpu
-    # memory    = each.value.task_definition.memory
+    name   = each.value.task_definition.name
+    image  = each.value.task_definition.image
+    cpu    = each.value.task_definition.cpu
+    memory = each.value.task_definition.memory
     # essential = true
     # #portMappings       = each.value.task_definition.portMappings
     # #environment        = each.value.task_definition.environment
