18a967b12d5d7acc760dc55cf522bc0f535e716c
Sibabalwe Ngqoyiya <sngqoyiya@plusonex.com>
2023-07-19 14:38:34 +0200

    element

--- task-definitions.tf
+++ task-definitions.tf
@@ -9,8 +9,8 @@ resource "aws_ecs_task_definition" "this" {
     cpu                = each.value.task_definition.cpu
     memory             = each.value.task_definition.memory
     essential          = true
-    portMappings       = each.value.task_definition.portMappings
-    environments       = each.value.task_definition.environment
+    portMappings       = element(each.value.task_definition.portMappings, 0)
+    environments       = element(each.value.task_definition.environment, 0)
     log_options_name   = each.value.task_definition.log_options.logs_group
     log_options_region = each.value.task_definition.log_options.region
   })
