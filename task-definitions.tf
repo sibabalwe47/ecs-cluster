d20ebef75a15877490d5ed1589c012e713072781
Sibabalwe Ngqoyiya <sngqoyiya@plusonex.com>
2023-07-19 11:39:46 +0200

    testing json encode

--- task-definitions.tf
+++ task-definitions.tf
@@ -9,8 +9,8 @@ resource "aws_ecs_task_definition" "this" {
     cpu                = each.value.task_definition.cpu
     memory             = each.value.task_definition.memory
     essential          = true
-    portMappings       = each.value.task_definition.portMappings
-    environment        = each.value.task_definition.environment
+    portMappings       = jsonencode(each.value.task_definition.portMappings)
+    environment        = jsonencode(each.value.task_definition.environment)
     log_options_name   = each.value.task_definition.log_options.logs_group
     log_options_region = each.value.task_definition.log_options.region
   })
