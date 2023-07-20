490b3b2751cfabc739f94ab8b87c1d3f57417d80
Sibabalwe Ngqoyiya <sngqoyiya@plusonex.com>
2023-07-20 14:03:53 +0200

    task_definition arn test

--- services.tf
+++ services.tf
@@ -1,7 +1,8 @@
 resource "aws_ecs_service" "this" {
-  for_each      = { for index, service in var.services : service.name => service }
-  name          = each.value.name
-  cluster       = each.value.cluster_id
-  desired_count = each.value.desired_count
-  launch_type   = var.runtime == "FARGATE" ? "FARGATE" : "EC2"
+  for_each        = { for index, service in var.services : service.name => service }
+  name            = each.value.name
+  cluster         = each.value.cluster_id
+  desired_count   = each.value.desired_count
+  launch_type     = var.runtime == "FARGATE" ? "FARGATE" : "EC2"
+  task_definition = aws_ecs_task_definition.this[var.task_definition_index].arn
 }
