15861ce2c9d654a1b694bb1acaa9bf1eaf7f0f46
Sibabalwe Ngqoyiya <sngqoyiya@plusonex.com>
2023-07-20 06:18:41 +0200

    added runtime platform

--- task-definitions.tf
+++ task-definitions.tf
@@ -18,4 +18,9 @@ resource "aws_ecs_task_definition" "this" {
     log_options_name   = each.value.task_definition.log_options.logs_group
     log_options_region = each.value.task_definition.log_options.region
   })
+
+  runtime_platform {
+    operating_system_family = "LINUX"
+    cpu_architecture        = "X86_64"
+  }
 }
