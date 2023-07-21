5d5a875e982931f7d13b580f9c2965c9b6e402b8
Sibabalwe Ngqoyiya <sngqoyiya@plusonex.com>
2023-07-21 08:15:00 +0200

    correct reference

--- ecr.tf
+++ ecr.tf
@@ -1,6 +1,7 @@
 resource "aws_ecr_repository" "this" {
-  for_each             = { for index, repository in var.ecr_repositories : repository.name => repository }
-  name                 = each.value.name
+  #   for_each             = { for index, repository in var.ecr_repositories : repository.name => repository }
+  count                = length(var.ecr_repositories) > 0 ? var.ecr_repositories : 0
+  name                 = var.ecr_repositories[count.index]
   force_delete         = false
   image_tag_mutability = "MUTABLE"
   image_scanning_configuration {
