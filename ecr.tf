b1a134572258545872e029f3b7d76ba89fecbe24
Sibabalwe Ngqoyiya <sngqoyiya@plusonex.com>
2023-07-21 08:08:40 +0200

    correct variable

--- ecr.tf
+++ ecr.tf
@@ -1,5 +1,5 @@
 resource "aws_ecr_repository" "this" {
-  for_each             = { for index, repository in var.ecr_repos : repository.name => repository }
+  for_each             = { for index, repository in var.ecr_repositories : repository.name => repository }
   name                 = each.value.name
   force_delete         = false
   image_tag_mutability = "MUTABLE"
